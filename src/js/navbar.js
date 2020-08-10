1af5cb8a788795122ba715c86e9336343d0736fa
Alessandro <alessandroadm@live.com>
2020-08-10 19:18:03 -0300

    style(index): adding background image
    now the about section has a background image

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,3 +1,4 @@
+
 const BTN_CLOSE_MENU = document.getElementById('btn-close-mobile-menu');
 const HEADER_MOBILE = document.getElementById('header-top-mobile');
 const BTN_OPEN_MENU = document.getElementById('btn-open-mobile-menu');
